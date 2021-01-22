17adc873c86cb3e2a6776d59df28d0b7fadd5faa
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-01-22 13:21:27 -0300

    Using pdfview with auctex

--- settings/latex-settings.el
+++ settings/latex-settings.el
@@ -11,6 +11,15 @@
   (setq TeX-auto-save t
         TeX-parse-self t
         reftex-plug-into-AUCTeX t)
-  :hook (LaTeX-mode . reftex-mode))
+  :hook (LaTeX-mode . reftex-mode)
+  :init  ;; to use pdfview with auctex
+   (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+         TeX-view-program-list '(("PDF Tools" TeX-pdf-tools-sync-view))
+         TeX-source-correlate-start-server t) ;; not sure if last line is neccessary
+   (setq TeX-source-correlate-method 'synctex) ; enable synctex
+   (setq TeX-source-correlate-mode t) ; enable text-source-correlate using synctex
+   ;; to have the buffer refresh after compilation
+   (add-hook 'TeX-after-compilation-finished-functions
+             #'TeX-revert-document-buffer))
 
 (provide 'latex-settings)
