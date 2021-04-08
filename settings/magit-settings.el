f385fdfc7efd2a321c2d14e2c1ab6718559dd811
Francisco Orlandini <francisco.orlandini@gmail.com>
2021-04-08 11:06:05 -0300

    added forge package

--- settings/magit-settings.el
+++ settings/magit-settings.el
@@ -5,5 +5,9 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-c b") 'magit-blame)
 
+(use-package forge
+  :after magit)
+
+(setq auth-sources '("~/.authinfo"))
 
 (provide 'magit-settings)
