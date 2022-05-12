b8feef3a09660519bed64460a2ef9069c3a27d72
Francisco Orlandini <francisco.orlandini@gmail.com>
2022-05-12 11:46:41 -0300

    magits early init + ghub package

--- settings/magit-settings.el
+++ settings/magit-settings.el
@@ -1,13 +1,24 @@
 
 (use-package magit
-  :defer 2)
+  :defer 0.5
+  :init     
+    (setq auth-source-debug 'trivia)
+  :config
+    (add-hook 'magit-process-find-password-functions 'magit-process-password-auth-source))
+
 
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-c b") 'magit-blame)
 
-(use-package forge
+
+(use-package ghub
+  :demand t
   :after magit)
 
-(setq auth-sources '("~/.authinfo"))
+(use-package forge
+  :demand t
+  :after ghub)
+
+(setq auth-sources '("~/.authinfo.gpg"))
 
 (provide 'magit-settings)
