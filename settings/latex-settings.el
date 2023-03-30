9d5ebbbc19d91411209032958dfe1ec9b0a7ade2
Francisco Orlandini <francisco.orlandini@gmail.com>
2023-03-30 13:32:30 -0300

    bibtex pckg

--- settings/latex-settings.el
+++ settings/latex-settings.el
@@ -24,4 +24,28 @@
    (add-hook 'TeX-after-compilation-finished-functions
              #'TeX-revert-document-buffer))
 
+(use-package bibtex
+  :config
+  (setq bibtex-autokey-year-length 4
+      bibtex-autokey-name-year-separator ""
+      bibtex-autokey-year-title-separator "-"
+      bibtex-autokey-titleword-separator "-"
+      bibtex-autokey-titlewords 2
+      bibtex-autokey-titlewords-stretch 1
+      bibtex-autokey-titleword-length 5)
+  ;; (setq bibtex-completion-bibliography '("~/orgmode/bibliography.bib")
+	;; bibtex-completion-notes-template-multiple-files "* ${author-or-editor}, ${title}, ${journal}, (${year}) :${=type=}: \n\nSee [[cite:&${=key=}]]\n"
+
+	;; bibtex-completion-additional-search-fields '(keywords)
+	;; bibtex-completion-display-formats
+	;; '((article       . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${journal:40}")
+	;;   (inbook        . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} Chapter ${chapter:32}")
+	;;   (incollection  . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${booktitle:40}")
+	;;   (inproceedings . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${booktitle:40}")
+	;;   (t             . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*}"))
+	;; bibtex-completion-pdf-open-function
+	;; (lambda (fpath)
+	;;   (call-process "open" nil 0 nil fpath)))
+  )
+
 (provide 'latex-settings)
