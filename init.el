13a65571ebf3f6594fd51d82863ffb6365ba855d
Francisco Orlandini <francisco.orlandini@gmail.com>
2023-04-19 21:04:43 -0300

    removing custom-set-variables from init.el repo

--- init.el
+++ init.el
@@ -34,27 +34,3 @@
 (require 'dwim-settings)
 ;; ;; End:
 ;; ;;; init.el ends here
-
-
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(c-noise-macro-names (quote ("constexpr")))
-;;  '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
-;;  '(org-trello-files (quote ("~/org/trello/ngs.org" "")) nil (org-trello))
-;;  '(package-selected-packages
-;;    (quote
-;;     (auctex crux company-quickhelp shx transient ccls eglot magit-popup gnu-elpa-keyring-update company-lsp cquery lsp-ui lsp-mode vterm use-package smex pdf-tools org-trello nord-theme move-text magit helm flycheck-pos-tip flycheck-color-mode-line elpy ein cmake-mode cff blacken)))
-;;  '(python-shell-interpreter "/usr/bin/python3")
-;;  '(safe-local-variable-values
-;;    (quote
-;;     ((TeX-command-extra-options . "-shell-escape")
-;;      (TeX-command-extra-optins . "-shell-escape")))))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
