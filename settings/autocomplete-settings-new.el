2271607b629071ceef6b0e8d1ac45d85e7b02d2f
Francisco Orlandini <francisco.orlandini@gmail.com>
2023-04-26 17:35:10 -0300

    adds projectile settings for autocomplete-settings-new

--- settings/autocomplete-settings-new.el
+++ settings/autocomplete-settings-new.el
@@ -24,5 +24,17 @@
 
 (use-package lsp-mode
   :disabled t)
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-x p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/git")
+    (setq projectile-project-search-path '("~/git")))
+  (setq projectile-switch-project-action #'projectile-dired))
 (provide 'autocomplete-settings-new)
 ;;; autocomplete-settings-new.el ends here
