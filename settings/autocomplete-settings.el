d2af0f3ab6154eaf6f0ed3c403e9ef6652d9d6bc
Francisco Orlandini <francisco.orlandini@gmail.com>
2024-06-25 08:49:15 -0300

    feat(flycheck): adds proselint as next checker

--- settings/autocomplete-settings.el
+++ settings/autocomplete-settings.el
@@ -160,6 +160,21 @@
   (setq lsp-pyright-extra-paths (projectile-project-root))
   )
 
+
+(defvar-local my/flycheck-local-cache nil)
+
+(defun my/flycheck-checker-get (fn checker property)
+  (or (alist-get property (alist-get checker my/flycheck-local-cache))
+      (funcall fn checker property))) 
+
+(advice-add 'flycheck-checker-get :around 'my/flycheck-checker-get)
+
+(add-hook 'lsp-managed-mode-hook
+          (lambda ()
+            (when (derived-mode-p 'tex-mode)
+              (flycheck-add-mode 'proselint 'latex-mode)
+              (setq my/flycheck-local-cache '((lsp . ((next-checkers . (proselint)))))))))
+
 ;; https://github.com/FredeEB/.emacs.d#yasnippet
 (use-package yasnippet
   :config
